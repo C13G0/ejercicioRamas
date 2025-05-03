a9c8561161aef7e7396d1f13e1fa7d2f6468df95
C13G0 <blindprogramming@gmail.com>
2025-05-03 11:51:32 -0500

    Importación clase Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Venta de sustancias de dudosa procedencia.....");
+
+
+
+
     }
 }
 
